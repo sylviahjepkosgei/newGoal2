c77734aa3832d36cd7662eb49eb682658f807148
sylviah <sylviah.rutto@gmail.com>
2019-07-04 13:09:31 +0300

    goals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'goal2';
+//   goals: Goal[] = [
+// new Goal(1,'Watch finding nemo','Find an online version and watch merlin find his son',new Date (2019,3,14) ),
+// new Goal(2,'Buy cookies','I have to buy cookies for the parrot',new Date (2019,6,9) ),
+// new Goal(3,'Get new phone case','Diana has her birthday coming up soon',new Date (2019,1,12 ),
+// new Goal(4,'Get dog food','pupper likes expensive sancks',new Date (2019,0,18) ),
+// new Goal(5,'Solve math homework','Damn math',new Date (2019,0,18) ),
+// new Goal(6,'plot my world domination plan','Cause Iam an evil overlord',new Date (2019,3,14) ),
+//   ];
 }
